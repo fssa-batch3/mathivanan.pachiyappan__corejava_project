bd46abbc903bed823999ab7384adae86726d60ca
NSP.Mathi <116268923+mathivanan8@users.noreply.github.com>
2023-07-12 17:39:35 +0530

    Day03 practice

---
+++ src/main/java/Day03/practice/Employee.java
@@ -0,0 +1,13 @@
+package Day03.practice;
+
+public class Employee {
+	 
+    public int id;
+	public String name;
+
+ 
+    public Employee(int newId, String newName) {
+        id = newId;
+        name = newName;
+    }
+}
---
+++ src/main/java/Day03/practice/User.java
@@ -0,0 +1,20 @@
+package Day03.practice;
+
+public class User {
+
+	String name;
+	String email;
+	int password;
+	
+	public User() {
+		
+		
+	}
+	
+	public User(String newName, String newEmail, int newPassword) {
+		
+		name = newName;
+		email = newEmail;
+		password = newPassword;
+	}
+}
